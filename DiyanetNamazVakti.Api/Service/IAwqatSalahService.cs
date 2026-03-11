c4f694a3d8dad00c96455a1b248fbf1f11407446
ottoman5886 <113133588+ottoman5886@users.noreply.github.com>
2026-03-11 11:00:51 +0100

    Update IAwqatSalahService.cs

--- DiyanetNamazVakti.Api/Service/IAwqatSalahService.cs
+++ DiyanetNamazVakti.Api/Service/IAwqatSalahService.cs
@@ -1,13 +1,11 @@
-﻿using PrayerTime.Service.Models;
-
+using PrayerTime.Service.Models;
 namespace DiyanetNamazVakti.Api.Service;
-
 public interface IAwqatSalahService
 {
-    Task<List<AwqatSalahModel>> DailyAwqatSalah(int cityId);
-    Task<List<AwqatSalahModel>> WeeklyAwqatSalah(int cityId);
-    Task<List<AwqatSalahModel>> MonthlyAwqatSalah(int cityId);
-    Task<List<AwqatSalahModel>> YearlyAwqatSalah(DateRangeFilter dateRange);
+    Task<List<AwqatSalahModel>> DailyAwqatSalah(int cityId, bool refresh = false);
+    Task<List<AwqatSalahModel>> WeeklyAwqatSalah(int cityId, bool refresh = false);
+    Task<List<AwqatSalahModel>> MonthlyAwqatSalah(int cityId, bool refresh = false);
+    Task<List<AwqatSalahModel>> YearlyAwqatSalah(DateRangeFilter dateRange, bool refresh = false);
     Task<EidAwqatSalahModel> EidAwqatSalah(int cityId);
     Task<List<AwqatSalahModel>> RamadanAwqatSalah(int cityId);
 }
