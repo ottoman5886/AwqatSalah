86b850662d487e2355b29356ce60f735002c1469
ottoman5886 <113133588+ottoman5886@users.noreply.github.com>
2026-03-11 10:51:44 +0100

    Update MyApiClientSettings.cs

--- DiyanetNamazVakti.Api/Core/Settings/MyApiClientSettings.cs
+++ DiyanetNamazVakti.Api/Core/Settings/MyApiClientSettings.cs
@@ -1,7 +1,8 @@
-﻿namespace DiyanetNamazVakti.Api.Core.Settings;
-
-public class MyApiClientSettings: IMyApiClientSettings
+namespace DiyanetNamazVakti.Api.Core.Settings;
+public class MyApiClientSettings : IMyApiClientSettings
 {
     public string UserName { get; set; }
     public string SecretCode { get; set; }
+    public string AdminName { get; set; }
+    public string AdminCode { get; set; }
 }
