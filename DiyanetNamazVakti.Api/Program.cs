88da7ba39f80bb82e5838e73b0c6708bcb0e1c1f
ottoman5886 <113133588+ottoman5886@users.noreply.github.com>
2026-03-11 23:02:03 +0100

    Update Program.cs

--- DiyanetNamazVakti.Api/Program.cs
+++ DiyanetNamazVakti.Api/Program.cs
@@ -1,11 +1,39 @@
 using Asp.Versioning.ApiExplorer;
 using DiyanetNamazVakti.Api.WebCommon.ActionFilters;
 using DiyanetNamazVakti.Api.WebCommon.Extensions;
+using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddCors(options => options.AddDefaultPolicy(builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()));
+
+// Rate Limiting → 50 Anfragen pro Minute pro API Key
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddPolicy("perApiKey", context =>
+    {
+        var apiKey = context.Request.Headers["X-API-Key"].ToString();
+        var key = string.IsNullOrEmpty(apiKey) ? context.Connection.RemoteIpAddress?.ToString() ?? "unknown" : apiKey;
+
+        return RateLimitPartition.GetFixedWindowLimiter(key, _ => new FixedWindowRateLimiterOptions
+        {
+            PermitLimit = 50,
+            Window = TimeSpan.FromMinutes(1),
+            QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+            QueueLimit = 0
+        });
+    });
+
+    options.OnRejected = async (context, token) =>
+    {
+        context.HttpContext.Response.StatusCode = 429;
+        context.HttpContext.Response.ContentType = "application/json";
+        await context.HttpContext.Response.WriteAsync(
+            "{\"statusCode\":429,\"message\":\"Zu viele Anfragen. Bitte warte eine Minute.\"}",
+            token);
+    };
+});
 
 // AwqatSalah Service Settings
 var awqatSalahSettings = builder.Configuration.GetSection(nameof(AwqatSalahSettings));
@@ -32,8 +60,7 @@ builder.Services.AddTransient<IDailyContentService, DailyContentService>();
 builder.Services.AddTransient<IAwqatSalahService, AwqatSalahService>();
 
 builder.Services
-    //.AddControllers()
-    .AddControllers(opt => opt.Filters.Add<ClientAtionFilter>()) //Use here to add a simple authentication It is recommended. Please delete the top line
+    .AddControllers(opt => opt.Filters.Add<ClientAtionFilter>())
     .ConfigureApiBehaviorOptions(options =>
     {
         options.InvalidModelStateResponseFactory = context =>
@@ -50,7 +77,7 @@ builder.Services
 //Api Versioning
 builder.Services.AddAndConfigureApiVersioning();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.ConfigureSwagger();
@@ -60,15 +87,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
 app.UseSwagger(apiVersionDescriptionProvider);
-
 app.UseMiddleware<ExceptionMiddleware>();
-
 app.UseCors();
-
 app.UseHttpsRedirection();
-
+app.UseRateLimiter();
 app.UseAuthorization();
-
-app.MapControllers();
-
+app.MapControllers().RequireRateLimiting("perApiKey");
 app.Run();
