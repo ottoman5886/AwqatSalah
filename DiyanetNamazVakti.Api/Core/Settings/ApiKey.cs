e0d45e02dbb0b5bda36f4249bde3f736cd90d7d0
ottoman5886 <113133588+ottoman5886@users.noreply.github.com>
2026-03-11 14:28:40 +0100

    Create ApiKey.cs

---
+++ DiyanetNamazVakti.Api/Core/Settings/ApiKey.cs
@@ -0,0 +1,8 @@
+namespace DiyanetNamazVakti.Api.Core.Settings;
+
+public class ApiKey
+{
+    public string Key { get; set; }
+    public string Name { get; set; }
+    public bool IsAdmin { get; set; }
+}
